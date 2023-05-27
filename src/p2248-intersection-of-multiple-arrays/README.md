68b8adc64eb123f7329238f9ad5f1dd8cc37b576
Ismael Pamplona <ismaelpamplona@hotmail.com>
2023-05-27 08:23:08 -0300

    p2248-intersection-of-multiple-arrays

---
+++ src/p2248-intersection-of-multiple-arrays/README.md
@@ -0,0 +1,10 @@
+# p2248_intersection_of_multiple_arrays
+[https://leetcode.com/problems/intersection-of-multiple-arrays/](https://leetcode.com/problems/intersection-of-multiple-arrays/)
+
+
+## First approach - hashing
+
+- n = nums.len()
+- time complexity: O(n)
+- space complexity: O(n)
+
---
+++ src/p2248-intersection-of-multiple-arrays/p2248-intersection-of-multiple-arrays.ts
@@ -0,0 +1,25 @@
+export function intersection(nums: number[][]): number[] {
+    let result: number[] = []
+
+    let map: Map<number, number> = new Map()
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums[i].length; j++) {
+            let entry = map.get(nums[i][j])
+            if (entry) {
+                map.set(nums[i][j], entry + 1)
+            } else {
+                map.set(nums[i][j], 1)
+            }
+        }
+    }
+    for (const k of map.keys()) {
+        let entry = map.get(k)
+        if (entry === nums.length) {
+            result.push(k)
+        }
+    }
+
+    result.sort((a, b) => a - b);
+    return result
+};
